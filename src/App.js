b2cbeccd2116648cd0191deb71ad8f3962b3df79
lovisasars <74593044+lovisasars@users.noreply.github.com>
2021-02-12 13:12:59 +0100

    updated app.js and html index with bootstrap and font links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Weather from "./Weather";
+import React from "react";
 
-function App() {
+import Header from "./Header";
+import Footer from "./Footer";
+import Forecast from "./Forecast";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-        <Weather city="Stockholm" />
-      </header>
+      <br />
+      <Header />
+      <h2>Stockholm</h2>
+      <h3>Monday 12:25</h3>
+      <h1>
+        <img
+          src="http://openweathermap.org/img/wn/02d@2x.png"
+          alt="weatherIcon"
+          id="icon"
+        />
+        <span id="temp-number">1</span>
+        <span className="Units">
+          <a href="#" id="celsius-link" className="active">
+            {" "}
+            °C
+          </a>{" "}
+          |
+          <a href="#" id="fahrenheit-link">
+            °F
+          </a>
+        </span>
+      </h1>
+      <p className="Hum-wind">
+        Humidity: <span id="humidity">86</span>% <br />
+        Windspeed: <span id="windspeed">1</span> km/h
+      </p>
+      <br />
+      <Forecast />
+      <Footer />
+      <br />
     </div>
   );
 }
-
-export default App;
