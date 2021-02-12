2023278638abbe138e245ee0f2b2478a248a74de
lovisasars <74593044+lovisasars@users.noreply.github.com>
2021-02-12 13:15:59 +0100

    added code to footer and header files

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <br />
+      <p>
+        <a href="https://github.com/lovisasars/My-project" target="_blank">
+          Open-source code
+        </a>{" "}
+        by Lovisa Sars
+      </p>
+    </div>
+  );
+}
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="Container">
+      <form id="search-city">
+        <input
+          type="text"
+          id="city-value"
+          placeholder="Enter your city"
+          className="col-form-label"
+        />{" "}
+        <input type="submit" value="Search" className="btn btn-light" />{" "}
+        <button type="button" id="location-button" className="btn btn-light">
+          Use my current location
+        </button>
+      </form>
+
+      <br />
+    </div>
+  );
+}
