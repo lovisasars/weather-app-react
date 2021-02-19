3c13321b3244e0c62bd1db44bee23f894c9db69a
lovisasars <74593044+lovisasars@users.noreply.github.com>
2021-02-19 13:02:07 +0100

    removed header file
    transferred all code to forecast file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-import Header from "./Header";
 import Footer from "./Footer";
 import Forecast from "./Forecast";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,28 +8,8 @@ export default function App() {
   return (
     <div className="App">
       <br />
-      <Header />
-      <h2>Stockholm</h2>
-      <h3>Monday 12:25</h3>
-      <h1>
-        <img
-          src="http://openweathermap.org/img/wn/02d@2x.png"
-          alt="weatherIcon"
-        />
-        1
-        <span className="Units">
-          <a href="#" className="active">
-            {" "}
-            °C
-          </a>{" "}
-          |<a href="#">°F</a>
-        </span>
-      </h1>
-      <p className="Hum-wind">
-        Humidity: 86 % <br />
-        Windspeed: 1 km/h
-      </p>
-      <br />
+      
+   
       <Forecast />
       <Footer />
       <br />
--- src/Forecast.js
+++ src/Forecast.js
@@ -2,7 +2,46 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Forecast() {
+ 
   return (
+
+    <div className="Container">
+      <form className="search-city">
+        <input
+          type="text"
+          placeholder="Enter your city"
+          className="col-form-label"
+        />{" "}
+        <input type="submit" value="Search" className="btn btn-light" />{" "}
+        <button type="button" className="btn btn-light">
+          Use my current location
+        </button>
+      </form>
+      <br />
+   <h2>Stockholm</h2>
+      <h3>Monday 12:25</h3>
+      <h1>
+        <img
+          src="http://openweathermap.org/img/wn/02d@2x.png"
+          alt="weatherIcon"
+        />
+        1
+        <span className="Units">
+          <a href="#" className="active">
+            {" "}
+            °C
+          </a>{" "}
+          |<a href="#">°F</a>
+        </span>
+      </h1>
+      <p className="Hum-wind">
+        Humidity: 86 % <br />
+        Windspeed: 1 km/h
+      </p>
+    
+      <br />
+    
+  
     <div className="Week">
       <div className="row">
         <div className="col-sm-2">
@@ -86,6 +125,7 @@ export default function Forecast() {
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 }
